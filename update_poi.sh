819d4064d8e16baec490fe7e08081efbc0e53251
Frederic Bonifas <fredericbonifas@gmail.com>
2015-05-06 12:18:11 +0200

    schools

--- update_poi.sh
+++ update_poi.sh
@@ -30,33 +30,64 @@ do
     echo_substep "${category} -> ${type}    ${key} -> ${value}"
     if [ ! -z "$key_exclude" ]
     then
-        # No polygon may be returned with some tags, e.g. public_transport_stop_position.
-        # In this case, the intersection should not be performed to avoid getting empty results.
-        sudo -n -u postgres -s -- psql -d ${POI_DB} -c "
-            INSERT INTO poi
-            SELECT osm_id, geom, name, '${category}', '${type}', '${key}', '${value}'
-            FROM dblink('dbname=${OSM_DB}',
-                        'WITH simplefeatures AS (
-                          SELECT ST_MakePolygon(ST_exteriorring((ST_Dump(way)).geom)) as geom FROM planet_osm_polygon
-                          WHERE \"${key}\"=''${value}''
-                            AND (${key_exclude} IS NULL OR ${key_exclude} != ''${value_exclude}'')
-                        ), poly AS (
-                          SELECT st_collect(geom) as geom, count(geom) FROM simplefeatures
-                        )
-                        SELECT osm_id, way, name FROM planet_osm_point, poly
-                        WHERE \"${key}\"=''${value}''
-                        AND (${key_exclude} IS NULL OR ${key_exclude} != ''${value_exclude}'')
-                        AND CASE WHEN count = 0 THEN true ELSE NOT ST_intersects(way,poly.geom) END;')
-                  AS t1(osm_id bigint, geom geometry,name text);"
-        sudo -n -u postgres -s -- psql -d ${POI_DB} -c "
-            INSERT INTO poi
-            SELECT osm_id, ST_Centroid(geom), name, '${category}', '${type}', '${key}', '${value}'
-            FROM dblink('dbname=${OSM_DB}',
-                        'SELECT osm_id, way, name
-                        FROM planet_osm_polygon
-                        WHERE \"${key}\"=''${value}''
-                        AND (${key_exclude} IS NULL OR ${key_exclude} != ''${value_exclude}'')')
-                  AS t1(osm_id bigint, geom geometry,name text);"
+        if [ ! -z "$value_exclude" ]
+        then
+            # No polygon may be returned with some tags, e.g. public_transport_stop_position.
+            # In this case, the intersection should not be performed to avoid getting empty results.
+            sudo -n -u postgres -s -- psql -d ${POI_DB} -c "
+                INSERT INTO poi
+                SELECT osm_id, geom, name, '${category}', '${type}', '${key}', '${value}'
+                FROM dblink('dbname=${OSM_DB}',
+                            'WITH simplefeatures AS (
+                              SELECT ST_MakePolygon(ST_exteriorring((ST_Dump(way)).geom)) as geom FROM planet_osm_polygon
+                              WHERE \"${key}\"=''${value}''
+                                AND (\"${key_exclude}\" IS NULL OR \"${key_exclude}\" != ''${value_exclude}'')
+                            ), poly AS (
+                              SELECT st_collect(geom) as geom, count(geom) FROM simplefeatures
+                            )
+                            SELECT osm_id, way, name FROM planet_osm_point, poly
+                            WHERE \"${key}\"=''${value}''
+                            AND (\"${key_exclude}\" IS NULL OR \"${key_exclude}\" != ''${value_exclude}'')
+                            AND CASE WHEN count = 0 THEN true ELSE NOT ST_intersects(way,poly.geom) END;')
+                      AS t1(osm_id bigint, geom geometry,name text);"
+            sudo -n -u postgres -s -- psql -d ${POI_DB} -c "
+                INSERT INTO poi
+                SELECT osm_id, ST_Centroid(geom), name, '${category}', '${type}', '${key}', '${value}'
+                FROM dblink('dbname=${OSM_DB}',
+                            'SELECT osm_id, way, name
+                            FROM planet_osm_polygon
+                            WHERE \"${key}\"=''${value}''
+                            AND (\"${key_exclude}\" IS NULL OR \"${key_exclude}\" != ''${value_exclude}'')')
+                      AS t1(osm_id bigint, geom geometry,name text);"
+        else 
+            # No polygon may be returned with some tags, e.g. public_transport_stop_position.
+            # In this case, the intersection should not be performed to avoid getting empty results.
+            sudo -n -u postgres -s -- psql -d ${POI_DB} -c "
+                INSERT INTO poi
+                SELECT osm_id, geom, name, '${category}', '${type}', '${key}', '${value}'
+                FROM dblink('dbname=${OSM_DB}',
+                            'WITH simplefeatures AS (
+                              SELECT ST_MakePolygon(ST_exteriorring((ST_Dump(way)).geom)) as geom FROM planet_osm_polygon
+                              WHERE \"${key}\"=''${value}''
+                                AND (\"${key_exclude}\" IS NULL)
+                            ), poly AS (
+                              SELECT st_collect(geom) as geom, count(geom) FROM simplefeatures
+                            )
+                            SELECT osm_id, way, name FROM planet_osm_point, poly
+                            WHERE \"${key}\"=''${value}''
+                            AND (\"${key_exclude}\" IS NULL)
+                            AND CASE WHEN count = 0 THEN true ELSE NOT ST_intersects(way,poly.geom) END;')
+                      AS t1(osm_id bigint, geom geometry,name text);"
+            sudo -n -u postgres -s -- psql -d ${POI_DB} -c "
+                INSERT INTO poi
+                SELECT osm_id, ST_Centroid(geom), name, '${category}', '${type}', '${key}', '${value}'
+                FROM dblink('dbname=${OSM_DB}',
+                            'SELECT osm_id, way, name
+                            FROM planet_osm_polygon
+                            WHERE \"${key}\"=''${value}''
+                            AND (\"${key_exclude}\" IS NULL)')
+                      AS t1(osm_id bigint, geom geometry,name text);"
+        fi
     else
         # No polygon may be returned with some tags, e.g. public_transport_stop_position.
         # In this case, the intersection should not be performed to avoid getting empty results.
